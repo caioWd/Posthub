cdfd7dd37613d6830a695e6976088bdcae5d6ca4
caioWd <caio.wdl@gmail.com>
2025-03-29 15:36:59 -0300

    fisrt commit

---
+++ src/main/java/com/myproj/posthub_backend/PosthubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myproj.posthub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosthubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosthubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproj/posthub_backend/PosthubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproj.posthub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosthubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
